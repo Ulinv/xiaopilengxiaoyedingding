2b5822faf6fd32541f65b45b14e02f0270e63df0
Ulinv <lushengdong123456789@gmail.com>
2025-02-02 14:11:57 +0800

    Add files via upload

---
+++ bapilengjmxkms.lua
@@ -0,0 +1,11 @@
+if ID == "e7ed410f-542f-4c7c-9241-6c23fbb379e0" then 
+JM = true 
+elseif ID == "672fb117-58df-4e6b-b9c9-c73d19977e05" then 
+JM = true 
+elseif ID == "aaa3d2d8-1be1-48e8-a9c3-57a62a3e12eb" then 
+JM = true 
+elseif ID == "aaa3d2d8-1be1-48e8-a9c3-57a62a3e12eb" then 
+JM = true 
+elseif ID == "aaa3d2d8-1be1-48e8-a9c3-57a62a3e15eb" then 
+JM = true 
+end
